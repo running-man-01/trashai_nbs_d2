574ca277bf2bb4e35c099bd99fbb68f247cbe90f
running-man-01 <112680312+running-man-01@users.noreply.github.com>
2022-10-06 16:56:22 -0400

    Update docker_env.sh

--- docker_env.sh
+++ docker_env.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-apt update && apt install git wget curl python3-pip -y && \
+apt update && apt install git wget curl python3-pip unzip -y && \
 pip install --upgrade pip && \
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata && \
 apt-get install ffmpeg libsm6 libxext6  -y && pip install opencv-contrib-python && \
 pip install --user torch==1.10 torchvision==0.11.1 -f https://download.pytorch.org/whl/cu111/torch_stable.html && \
-pip install jupyterlab && \
-pip install --upgrade pandas seaborn && \
+pip install --upgrade jupyterlab pandas seaborn gdown jupyter ipywidgets&& \
 python3 -m pip install 'git+https://github.com/facebookresearch/detectron2.git' && \
 git clone https://github.com/running-man-01/trashai_nbs_d2 && \
 cd trashai_nbs_d2 && \
